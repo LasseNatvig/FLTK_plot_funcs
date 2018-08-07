80239319be6170b0c84962771379805cd06cd397
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-07 10:02:17 +0200

    fjerner mer kode, prøver tegne enkel polyline først

--- FLTK_base/main.cpp
+++ FLTK_base/main.cpp
@@ -40,74 +40,9 @@ constexpr int ylength = ymax - yoffset - yspace;
 typedef double Fcti(double);
 double mysin(double x) { return sin(x); }
 
-template <class T> class myFct : public Shape {
-public:
-	myFct(Fcti* f, double r1, double r2, Point orig, int count = 100, double xscale = 25, double yscale = 25, T precision = 1.0);
-	~myFct() {}
-
-	void set_f(Fcti* p) { f = p; }
-	void set_r1(double r) { r1 = r; }
-	void set_r2(double r) { r2 = r; }
-	void set_orig(Point o) { orig = o; }
-	void set_count(int c) { count = c; }
-	void set_xscale(double x) { xscale = x; }
-	void set_yscale(double y) { yscale = y; }
-	void set_precision(T p) { precision = p; }
-
-	double get_r1() { return r1; }
-	double get_r2() { return r2; }
-	Point get_orig() { return orig; }
-	int get_count() { return count; }
-	double get_xscale() { return xscale; }
-	double get_yscale() { return yscale; }
-	T get_precision() { return precision; }
-
-	void calc();
-	void reset(Fcti* f, double r1, double r2, Point orig, int count = 100, double xscale = 25, double yscale = 25, T precision = 1.0);
-
-private:
-	Fcti * f;
-	double r1;
-	double r2;
-	Point orig;
-	int count;
-	double xscale;
-	double yscale;
-	T precision;
-};
-template <class T> myFct<T>::myFct(Fcti* f, double r1, double r2, Point orig, int count, double xscale, double yscale, T precision)
-	:f(f), r1(r1), r2(r2), orig(orig), count(count), xscale(xscale), yscale(yscale), precision(precision)
-{
-	calc();
-}
-template <class T> void myFct<T>::calc() {
-	if (r2 - r1 <= 0) error("bad graphing range");
-	if (count <= 0) error("non-positive graphing count");
-	double dist = (r2 - r1) / count;
-	double r = r1;
-
-	for (int i = 0; i < count; ++i) {
-		int x = orig.x + static_cast<int>(int(int(r * xscale) / precision) * precision);
-		int y = orig.y - static_cast<int>(int(int(f(r) * yscale) / precision) * precision);
-		add(Point{ x, y });
-		r += dist;
-	}
-}
-template <class T> void myFct<T>::reset(Fcti* f, double r1, double r2, Point orig, int count, double xscale, double yscale, T precision) {
-	set_f(f);
-	set_r1(r1);
-	set_r2(r2);
-	set_orig(orig);
-	set_count(count);
-	set_xscale(xscale);
-	set_yscale(yscale);
-	set_precision(precision);
-	calc();
-}
-
 struct app_window : Window {
 	enum graph_enum {
-		sin_g, cos_g
+		sin_g, poly_g
 	};
 	app_window(Point xy, int w, int h, const string& title);
 	void draw_graph();
@@ -122,61 +57,61 @@ private:
 	graph_enum g_type;
 	Axis x_axis;
 	Axis y_axis;
-	myFct<double> funct;
-
-	//double x_manipulation(double x, int m) { return x * m; }
+	Open_polyline func_toPlot;  // Changed by Lasse
 	double my_sin(double x) { return sin(x); }
-
 	// actions invoked by callbacks
 	void quit() { hide(); }
-
 	void graph_type_menu_pressed() { 
 		graph_type.hide(); 
 		graph_type_menu.show(); 
 	}
 	void sin_graph();
-
 	// callback functions
 	static void cb_quit(Address, Address pw) { 
 		reference_to<app_window>(pw).quit(); 
 	}
-	static void cb_calculate(Address, Address pw) { reference_to<app_window>(pw).draw_graph(); }
-	static void cb_graph_type_menu_pressed(Address, Address pw) { reference_to<app_window>(pw).graph_type_menu_pressed(); }
-	static void cb_sin_graph(Address, Address pw) { reference_to<app_window>(pw).sin_graph(); }
-
+	static void cb_calculate(Address, Address pw) { 
+		reference_to<app_window>(pw).draw_graph(); 
+	}
+	static void cb_graph_type_menu_pressed(Address, Address pw) { 
+		reference_to<app_window>(pw).graph_type_menu_pressed(); 
+	}
+	static void cb_sin_graph(Address, Address pw) { 
+		reference_to<app_window>(pw).sin_graph(); 
+	}
 };
 
 app_window::app_window(Point xy, int w, int h, const string& title) : Window(xy, w, h, title),
-	quit_button(Point{ x_max() - 80, 10 }, 70, 20, "Quit", cb_quit),
-	show_button(Point{ x_max() - 80, 35 }, 70, 20, "Show", cb_calculate),
-	graph_type(Point{ 20, 10}, 100, 20, "Graph Type", cb_graph_type_menu_pressed),
-	graph_type_menu(Point{ 20, 0 }, 100, 20, Menu::vertical, "Graph Type"),
-	base_equation(Point{ x_max() / 2, 0 }, 90, 20, "equation"),
-	x_parameter(Point{100, 40}, 20, 20, "t-paramter"),
-	equation(Point{ x_max() / 2, 40 }, 90, 20, "equation"),
-	x_axis(Axis::x, Point{ orig.x - 200, orig.y }, 400, 20, "1 == 20"),
-	y_axis(Axis::y, Point{ orig.x, orig.y + 200 }, 400, 20, "1 == 20"),
-	funct(mysin, r_min, r_max, orig, n_points, x_scale, y_scale) {
+quit_button(Point{ x_max() - 80, 10 }, 70, 20, "Quit", cb_quit),
+show_button(Point{ x_max() - 80, 35 }, 70, 20, "Show", cb_calculate),
+graph_type(Point{ 20, 10 }, 100, 20, "Graph Type", cb_graph_type_menu_pressed),
+graph_type_menu(Point{ 20, 0 }, 100, 20, Menu::vertical, "Graph Type"),
+base_equation(Point{ x_max() / 2, 0 }, 90, 20, "equation"),
+x_parameter(Point{ 100, 40 }, 20, 20, "t-parameter"),
+equation(Point{ x_max() / 2, 40 }, 90, 20, "equation"),
+x_axis(Axis::x, Point{ orig.x - 200, orig.y }, 400, 20, "1 == 20"),
+y_axis(Axis::y, Point{ orig.x, orig.y + 200 }, 400, 20, "1 == 20"),
+// funct(mysin, r_min, r_max, orig, n_points, x_scale, y_scale) 
+func_toPlot() // New code shape
+{
 	attach(quit_button);
 	attach(show_button);
 	attach(graph_type);
 	attach(base_equation);
 	attach(x_parameter);
 	attach(equation);
-
-	graph_type_menu.attach(new Button(Point{ 0, 0 }, 0, 0, "sin", cb_sin_graph));
-
+	// NOTE: Menu is using template-class Vector_ref see paragraph 13.10
+	// TODO hide it in a .h file (since early in course and it uses new ... )
+	graph_type_menu.attach(new Button(Point{ 0, 0 }, 0, 0, "sin", cb_sin_graph)); 
+	graph_type_menu.attach(new Button(Point{ 0, 0 }, 0, 0, "poly", cb_sin_graph)); // TODO shows two buttons in menu, but poly is NOT impelmented
 	attach(graph_type_menu);
-
 	x_axis.set_color(Color::black);
 	y_axis.set_color(Color::black);
 	attach(x_axis);
 	attach(y_axis);
-
-	funct.set_color(FL_BLACK);
-	attach(funct);
-	funct.set_color(Color::invisible);
-
+	func_toPlot.set_color(FL_BLACK);
+	attach(func_toPlot);
+	func_toPlot.set_color(Color::invisible);
 	graph_type_menu.hide();
 }
 
@@ -190,31 +125,12 @@ void app_window::sin_graph() {
 void app_window::draw_graph() {
 	if (g_type == sin_g){
 		equation.put("y = sin(x) * " + x_parameter.get_string());
-		// most painful lambda to function pointer work-around ever.
-		// saved the day:
-		// https://deviorel.wordpress.com/2015/01/27/obtaining-function-pointers-from-lambdas-in-c/
-
-		int t = x_parameter.get_int();
-		auto la = [=](double x) { return sin(x) * t; };
-
-		static function< double(double) > static_variable;
-		static_variable = la;
-		double(*ptr)(double) = [](double x) { return static_variable(x); };
-
-		funct.reset(
-			[](double x) { return static_variable(x); }
-			, r_min
-			, r_max
-			, orig
-			, n_points
-			, x_scale
-			, y_scale
-		);
+		... legg inn enkel polylineeksempel fra boka her 
+		cout << " add code here for shape funct ... \n";
 	}
-	funct.set_color(Color::visible);
+	//funct.set_color(Color::visible);
 	redraw();
 }
-
 int main() {
 	app_window win(Point{ 100, 100 }, xmax, ymax + 20, "Text and graphics demo");
 	return gui_main();
